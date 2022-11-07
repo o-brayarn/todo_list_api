6c1ad34a828b705bc824d649b437957f01dcfce2
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-11-07 12:08:44 +0300

    completed a sample api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,60 @@
 class ApplicationController < Sinatra::Base
-    get '/' do # this is the root route of the application (the homepage) but you can have as many routes as you want
-        {hello: "Just a starting code 😃"}.to_json
-    end
+  set :default_content_type, "application/json" #Not compulsory
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = "*"
+  end
+
+  #enable CORS prefLight requests
+  options "*" do
+    response.headers['Access-Control-Allow-Method'] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  end
+
+  get "/" do # this is the root route of the application (the homepage) but you can have as many routes as you want
+    { hello: "Just a starting code 😃" }.to_json
+  end
+
+  get "/categories" do
+    Category.all.to_json
+  end
+
+  get "/category/:id" do #to  make it dynamic, hence accessible using an id
+    Category.find(params[:id]).to_json
+  end
+
+  get "/todo_lists" do
+    TodoList.all.to_json
+  end
+
+  get "/todo_list/:id" do
+    TodoList.find(params[:id]).to_json
+    # TodoList.find(title: params[:title]).to_json
+  end
+
+  post "/todo_list" do
+    todo_list = TodoList.create(
+      title: params[:title],
+      description: params[:description],
+      category_id: params[:category_id],
+      status: false,
+    )
+  end
+
+  patch "/todo_list/:id" do
+    todo_list = TodoList.find(params[:id])
+    todo_list.update(
+      title: params[:title],
+      description: params[:description],
+      category_id: params[:category_id],
+      status: params[:status],
+    )
+    # todo_list.to_json
+    {message: "Todo List was updated"}.to_json
+  end
+
+  delete "/todo_list/:id" do 
+    todo_list = TodoList.find(params[:id])
+    todo_list.destroy
+    {message: "Todo List was removed('#{todo_list.title}')"}.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 puts "🌱 Seeding data..."
 
 # place your seeds here
+Category.create(name: "Chores")
+Category.create(name: "Code challenge")
+Category.create(name: "code wars")
+Category.create(name: "Hike")
+Category.create(name: "watch documentaries")
 
+Category.all.each do |category|
+    6.times do
+        TodoList.create(
+            title: Faker::Lorem.sentence(word_count: 5),
+            description:Faker::Lorem.sentence(word_count: 20),
+            category_id:category.id,
+            status: false
+        )
+    end
+end
 puts "🌱 Done seeding!"
